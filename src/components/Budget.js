1bf166a2d3b240db586abaaf90ed34b1ddd3cd01
ALI-Ham-moud <ali_hadi_hamoud@hotmail.com>
2023-02-27 13:37:51 -0500

    final project!!

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,37 @@
-
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+	const { budget,dispatch,expenses, currency } = useContext(AppContext);
+
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+        if(val > 20000) {
+            alert("The value cannot exceed 20000");
+            //setCost("");
+            return;}
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+			}
+	}
+    
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
+            <input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
         </div>
     );
 };
+
 export default Budget;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,8 +3,8 @@ import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
-    const { expenses } = useContext(AppContext);
-    
+    const { expenses, } = useContext(AppContext);
+
     return (
         <table className='table'>
               <thead className="thead-light">
@@ -12,6 +12,7 @@ const ExpenseList = () => {
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>
               <th scope="col">Delete</th>
             </tr>
           </thead>
